2333ef4ec98400e84eec7518ea645051d006e115
Marcos Kloss <marcoskloss2001@gmail.com>
2022-08-29 18:06:54 -0300

    toggle Todo does not work anymore, why? refs: https://dev.to/ag-grid/react-18-avoiding-use-effect-getting-called-twice-4i9e https://www.techiediaries.com/react-18-useeffect/

--- challenge-p1/todolist/src/main.tsx
+++ challenge-p1/todolist/src/main.tsx
@@ -5,5 +5,7 @@ import App from "./App";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
