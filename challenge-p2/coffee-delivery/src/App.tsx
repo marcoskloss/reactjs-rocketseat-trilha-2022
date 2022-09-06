2d7a7f1178b85121f8e2de1e70b283c3ebbcfee0
Marcos Kloss <marcoskloss2001@gmail.com>
2022-09-06 11:01:54 -0300

    cp2: configure default styles

--- challenge-p2/coffee-delivery/src/App.tsx
+++ challenge-p2/coffee-delivery/src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/defaultTheme'
+
 export function App() {
-  return <h1>HELLO LADS</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
